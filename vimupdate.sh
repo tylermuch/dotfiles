45e7d8cbf902bf8ab994355cdd144f739d426a5e
Tyler Much <tylermuch@gmail.com>
2018-03-31 11:32:51 -0700

    Add comments for vim plugins

--- vimupdate.sh
+++ vimupdate.sh
@@ -1,20 +1,74 @@
 #!/usr/bin/env bash
 
 repos=(
-    gnattishness/cscope_maps
-    tpope/vim-fugitive
+    # vim-pathogen
+    #     vim plugin manager
+    
     tpope/vim-pathogen
+    # vim-fugitive
+    #     git commands
+    #   
+    #     :Gdiff
+    #     :Gblame
+    tpope/vim-fugitive
+
+    # vim-commentary
+    #     comment lines
+    #
+    #     gcc - toggle comment
+    #     gc - toggle comment on visual selection
     tpope/vim-commentary
+
+    # nerdtree
+    #     file explorer pane
+    #
+    #     :NERDTreeToggle
     scrooloose/nerdtree
-    jistr/vim-nerdtree-tabs
+
+    # vim-airline
+    #     nice looking and useful status line
     vim-airline/vim-airline
+
+    # jellybeans
+    #     my favorite theme
     nanotech/jellybeans.vim
+
+    # tagbar
+    #     browse symbols in file
+    #
+    #     :Tagbar   nmap = F8
     majutsushi/tagbar
+
+    # vim-multiple-cursors
+    #     sublime text style multiple cursors
+    #
+    #     Ctrl-n to select current word, repeat to select next occurances.
+    #     Ctrl-p to undo selection
+    #     Ctrl-x to skip to next possible selection
     terryma/vim-multiple-cursors
+
+    # fzf.vim
+    #     fzf in vim
+    #
+    #     :Files      Search file names, nmap = ;
+    #     :Buffers    Search vim buffers, nmap = b
+    #     :Lines      Search lines in buffer, nmap = l
+    #     :Ag         Search file content, nmap = f
     junegunn/fzf.vim
-    mileszs/ack.vim
+
+    # vim-polyglot
+    #     collection of language packs for syntax highlighting
     sheerun/vim-polyglot
+
+    # vim-airline-themes
+    #     airline themes
     vim-airline/vim-airline-themes
+
+    # cscope_maps
+    #     cscope tags
+    #
+    #     Ctrl-\ s    View references of symbol
+    gnattishness/cscope_maps
 )
 
 set -e
