bb2244a868c3a7ac883dd264b800e8f5512027a5
Tyler Much <tylermuch@gmail.com>
2018-04-06 02:23:25 -0700

    Make brew installs faster
    Run `brew install` once

--- brew.sh
+++ brew.sh
@@ -5,24 +5,24 @@ if [[ $(command -v brew) == "" ]]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew install ssh-copy-id
-brew install git
-brew install tig
-brew install wget
-brew install watch
-brew install ctags
-brew install the_silver_searcher
-brew install tree
-brew install fzf
-brew install colordiff
-brew install vim
-brew install tmux
-brew install ansifilter
-brew install zsh
+brew install ssh-copy-id            \
+             git                    \
+             tig                    \
+             wget                   \
+             watch                  \
+             ctags                  \
+             the_silver_searcher    \
+             tree                   \
+             fzf                    \
+             colordiff              \
+             vim                    \
+             tmux                   \
+             ansifilter             \
+             zsh
 
-brew cask install --appdir="/Applications" caffeine
-brew cask install --appdir="/Applications" alfred
-brew cask install --appdir="/Applications" sublime-text
+brew cask install --appdir="/Applications" caffeine     \
+                                           alfred       \
+                                           sublime-text 
 mkdir ~/bin
 ln -sf "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" $HOME/bin/subl
 
