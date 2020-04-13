b47149e22aa7315cd7dc2b4a666876dac6e81267
Tyler Much <tylermuch@gmail.com>
2020-04-13 15:12:59 -0700

    Install mosh

--- brew.sh
+++ brew.sh
@@ -20,7 +20,8 @@ brew install ssh-copy-id            \
              ansifilter             \
              reattach-to-user-namespace \
              zsh                    \
-             goto
+             goto                   \
+             mosh
 
 brew cask install --appdir="/Applications" caffeine     \
                                            alfred       \
@@ -32,5 +33,16 @@ brew install tag-ag
 
 mkdir ~/bin
 ln -sf "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" $HOME/bin/subl
+
+# mosh-server firewall setup. Configuring this through the preferences GUI doesn't seem to work properly
+# From https://github.com/mobile-shell/mosh/issues/898#issuecomment-368333946
+alias firepower='sudo /usr/libexec/ApplicationFirewall/socketfilterfw'
+MOSH_SYMLINK=$(which mosh-server)
+MOSH_CELLAR=$(brew --cellar mosh)/$(brew list --versions mosh | cut -d' ' -f2)/bin/mosh-server
+firepower --setglobalstate off
+firepower --add ${MOSH_SYMLINK}
+firepower --unblockapp ${MOSH_SYMLINK}
+firepower --add ${MOSH_CELLAR}
+firepower --unblockapp ${MOSH_CELLAR}
 
 brew cleanup
