53defd0f0bba157b2b37debb038752a95e5b0d7e
Tyler Much <tylermuch@gmail.com>
2020-04-19 16:21:58 -0700

    Add comments and fix up brew.sh

--- brew.sh
+++ brew.sh
@@ -5,25 +5,25 @@ if [[ $(command -v brew) == "" ]]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew install ssh-copy-id            \
-             git                    \
-             tig                    \
-             wget                   \
-             watch                  \
-             ctags                  \
-             ripgrep                \
-             tree                   \
-             fzf                    \
-             colordiff              \
-             vim                    \
-             tmux                   \
-             ansifilter             \
-             reattach-to-user-namespace \
-             zsh                    \
-             goto                   \
-             mosh                   \
-             fd                     \
-             the_silver_searcher
+brew install ssh-copy-id                 `# Copy ssh public key to remote system` \
+             git                         `# Git` \
+             tig                         `# Terminal based git browser` \
+             wget                        `# Wget` \
+             watch                       `# Periodically execute shell command` \
+             ctags                       `# Generate source code tags for quick navigation` \
+             ripgrep                     `# Much faster 'grep'` \
+             tree                        `# Pretty print filesystem trees` \
+             fzf                         `# Fuzzy file searcher` \
+             colordiff                   `# Diff in color` \
+             vim                         `# Vim` \
+             tmux                        `# Tmux` \
+             ansifilter                  `# Filter non-ascii characters` \
+             zsh                         `# Preferred shell` \
+             goto                        `# Quick file directory aliasing/navigation` \
+             mosh                        `# UDP-based ssh. Good for unstable connections.` \
+             fd                          `# Faster/prettier 'find'` \
+             the_silver_searcher         `# Faster grep. Used for :Ag in vim until I figure out :Rg...` \
+             bat                         `# Better 'cat'`
 
 brew cask install --appdir="/Applications" caffeine     \
                                            alfred       \
@@ -38,13 +38,12 @@ ln -sf "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" $HOME/bi
 
 # mosh-server firewall setup. Configuring this through the preferences GUI doesn't seem to work properly
 # From https://github.com/mobile-shell/mosh/issues/898#issuecomment-368333946
-alias firepower='sudo /usr/libexec/ApplicationFirewall/socketfilterfw'
 MOSH_SYMLINK=$(which mosh-server)
 MOSH_CELLAR=$(brew --cellar mosh)/$(brew list --versions mosh | cut -d' ' -f2)/bin/mosh-server
-firepower --setglobalstate off
-firepower --add ${MOSH_SYMLINK}
-firepower --unblockapp ${MOSH_SYMLINK}
-firepower --add ${MOSH_CELLAR}
-firepower --unblockapp ${MOSH_CELLAR}
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate off
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --add ${MOSH_SYMLINK}
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --unblockapp ${MOSH_SYMLINK}
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --add ${MOSH_CELLAR}
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --unblockapp ${MOSH_CELLAR}
 
 brew cleanup
