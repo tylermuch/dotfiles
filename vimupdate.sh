795a79a3a341d7d8af92c3e89cdb94a01abd002f
Tyler Much <tylermuch@gmail.com>
2021-09-18 15:55:15 -0700

    Categorize plugins in vimupdate.sh

--- vimupdate.sh
+++ vimupdate.sh
@@ -3,8 +3,9 @@
 repos=(
     # vim-pathogen
     #     vim plugin manager
-    
     tpope/vim-pathogen
+
+### Git-related plugins
     # vim-fugitive
     #     git commands
     #   
@@ -12,13 +13,11 @@ repos=(
     #     :Gblame
     tpope/vim-fugitive
 
-    # vim-commentary
-    #     comment lines
-    #
-    #     gcc - toggle comment
-    #     gc - toggle comment on visual selection
-    tpope/vim-commentary
+    # vim-gitgutter
+    #     Mark modified lines in the left gutter next to line numbers
+    airblade/vim-gitgutter
 
+### UI-related plugins
     # nerdtree
     #     file explorer pane
     #
@@ -33,11 +32,50 @@ repos=(
     #     my favorite theme
     nanotech/jellybeans.vim
 
+    # vim-polyglot
+    #     collection of language packs for syntax highlighting
+    sheerun/vim-polyglot
+
     # tagbar
     #     browse symbols in file
     #
     #     :Tagbar   nmap = F8
     majutsushi/tagbar
+
+    # vim-airline-themes
+    #     airline themes
+    vim-airline/vim-airline-themes
+
+    # indentLine
+    #     Display indentation markers, per 'ts'
+    Yggdroot/indentLine
+
+### FZF-related plugins
+    # fzf.vim
+    #     fzf in vim
+    #
+    #     :Files      Search file names
+    #     :Buffers    Search vim buffers
+    #     :Lines      Search lines in buffer
+    #     :Ag         Search file content
+    #
+    #     See mappings in .vimrc
+    junegunn/fzf
+    junegunn/fzf.vim
+
+    # fzf-tags
+    #     search for ctags using fzf
+    #     :FZFTags
+    #     :FZFTselect
+    zackhsi/fzf-tags
+
+### Utility plugins
+    # vim-commentary
+    #     comment lines
+    #
+    #     gcc - toggle comment
+    #     gc - toggle comment on visual selection
+    tpope/vim-commentary
 
     # vim-multiple-cursors
     #     sublime text style multiple cursors
@@ -47,43 +85,9 @@ repos=(
     #     Ctrl-x to skip to next possible selection
     terryma/vim-multiple-cursors
 
-    # fzf.vim
-    #     fzf in vim
-    #
-    #     :Files      Search file names, nmap = ;
-    #     :Buffers    Search vim buffers, nmap = b
-    #     :Lines      Search lines in buffer, nmap = l
-    #     :Ag         Search file content, nmap = f
-    junegunn/fzf
-    junegunn/fzf.vim
-
-    # vim-polyglot
-    #     collection of language packs for syntax highlighting
-    sheerun/vim-polyglot
-
-    # vim-airline-themes
-    #     airline themes
-    vim-airline/vim-airline-themes
-
-    # cscope_maps
-    #     cscope tags
-    #
-    #     Ctrl-\ s    View references of symbol
-    # conflicts with fzf-tags
-    # gnattishness/cscope_maps
-
-    # goyo
-    #     hide all vim window elements
-    #
-    #     :Goyo   nmap = F7
-    junegunn/goyo.vim
-
-    # limelight
-    #     highlight only current block/paragraph of code/text
-    #
-    #     :Limelight  nmap = F6
-    junegunn/limelight.vim
-
+    # vim-ripgrep
+    #     search via ripgrep
+    #     :Rg
     jremmen/vim-ripgrep
 
     # vim-unimpaired
@@ -93,14 +97,6 @@ repos=(
     #     [e Exchange current line with above
     #     ]e Exchange current line with below
     tpope/vim-unimpaired
-
-    # indentLine
-    #     Display indentation markers, per 'ts'
-    Yggdroot/indentLine
-
-    # vim-gitgutter
-    #     Mark modified lines in the left gutter next to line numbers
-    airblade/vim-gitgutter
 
     # vim-sneak
     #     Faster navigation
@@ -115,12 +111,6 @@ repos=(
     # vim-gutentags
     #     (re)generate tag files automatically
     ludovicchabant/vim-gutentags
-
-    # fzf-tags
-    #     search for ctags using fzf
-    #     :FZFTags
-    #     :FZFTselect
-    zackhsi/fzf-tags
 )
 
 set -e
