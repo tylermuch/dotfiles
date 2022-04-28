2b972e2417c354874609e45bbb2dc04f1079955c
Tyler Much <tylermuch@gmail.com>
2022-04-28 10:08:25 -0700

    Add diagnostics info to lualine status line

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,7 +1,29 @@
 local nvim_lsp = require "lspconfig"
 local lsp_status = require("lsp-status")
 
-require('lualine').setup()
+require('lualine').setup {
+  sections = {
+    lualine_b = {
+      'branch',
+      'diff',
+      {
+        'diagnostics',
+        sources = { 'nvim_diagnostic' },
+        sections = { 'error', 'warn', 'info', 'hint' },
+        diagnostics_color = {
+          error = 'DiagnosticError',
+          warn  = 'DiagnosticWarn',
+          info  = 'DiagnosticInfo',
+          hint  = 'DiagnosticHint',
+        },
+        symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' },
+        colored = true,
+        update_in_insert = false,
+        always_visible = false,
+      },
+    }
+  }
+}
 
 local on_attach = function(client, bufnr)
   lsp_status.register_progress()
