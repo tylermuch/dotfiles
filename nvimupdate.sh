2ce5a3b6f0b669db6da987334f6dc24995c122cc
Tyler Much <tylermuch@gmail.com>
2022-05-02 08:03:30 -0700

    Install coc-clangd during nvim setup

--- nvimupdate.sh
+++ nvimupdate.sh
@@ -4,3 +4,4 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 nvim --noplugin -c 'PlugInstall'
+nvim -c 'CocInstall coc-clangd'
