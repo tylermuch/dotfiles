6dcce747cf99efba40b4fa2ab862417ecf3405a6
Tyler Much <tylermuch@gmail.com>
2024-03-03 12:00:21 -0800

    Add coreutils and pv to brew install list

--- brew.sh
+++ brew.sh
@@ -27,7 +27,9 @@ brew install ssh-copy-id                 `# Copy ssh public key to remote system
              exa                         `# Better 'ls'` \
              neovim                      `# Neovim` \
              nodejs                      `# For neovim plugins` \
-             pyenv                       `# Multiple python installations`
+             pyenv                       `# Multiple python installations` \
+             coreutils                   `# GNU coreutils. Mostly for 'timeout'` \
+             pv                          `# Monitor progress of data through a pipe`
 
 brew cask install --appdir="/Applications" caffeine      \
                                            alfred        \
