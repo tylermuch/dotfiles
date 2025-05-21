1d757a880d56f9c7075aa72c11f810b9aedd2839
Tyler Much <tylermuch@gmail.com>
2025-05-20 23:16:40 -0700

    Show number of lines folded

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -114,7 +114,41 @@ require('highlight_current_n').setup({
 
 require('neoclip').setup()
 require("toggleterm").setup()
-require('ufo').setup()
+
+local ufo_fold_handler = function(virtText, lnum, endLnum, width, truncate)
+    local newVirtText = {}
+    local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+    local sufWidth = vim.fn.strdisplaywidth(suffix)
+    local targetWidth = width - sufWidth
+    local curWidth = 0
+    for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+        else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, {chunkText, hlGroup})
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+        end
+        curWidth = curWidth + chunkWidth
+    end
+    table.insert(newVirtText, {suffix, 'MoreMsg'})
+    return newVirtText
+end
+
+-- global handler
+-- `handler` is the 2nd parameter of `setFoldVirtTextHandler`,
+-- check out `./lua/ufo.lua` and search `setFoldVirtTextHandler` for detail.
+require('ufo').setup({
+    fold_virt_text_handler = ufo_fold_handler
+})
 
 -- nvim-ufo plugin configuration
 -- https://github.com/kevinhwang91/nvim-ufo
