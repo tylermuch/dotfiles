78592d88806a979ee72664cd2b7343764e56b6a2
Tyler Much <tylermuch@gmail.com>
2025-07-06 21:48:17 +0800

    Enable wrap

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,5 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_picker = "fzf"
-vim.opt.relativenumber = false
 vim.g.autoformat = false
+vim.opt.relativenumber = false
+vim.opt.wrap = true
