7a737c63e00621df17c740950dd51f98d6f1f93b
Tyler Much <tylermuch@gmail.com>
2025-07-13 08:04:51 -0700

    Disable automatic header insertion from the clangd lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,5 +2,11 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    autoformat  = false,
+    setup = {
+      clangd = function (_, opts)
+      opts.cmd = {"clangd", "--header-insertion=never"}
+      end,
+    }
   },
 }
