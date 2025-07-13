2fd3a986da9ce4960a98c494bef00ab6e7e958cd
Tyler Much <tylermuch@gmail.com>
2025-07-13 08:04:39 -0700

    Add old key bindings for fzf searching

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<localleader>f", ":FzfLua grep<Enter>")
+vim.keymap.set("n", "<localleader>;", ":FzfLua files<Enter>")
+vim.keymap.set("n", "<localleader>s", ":FzfLua grep_cword<Enter>")
+vim.keymap.set("n", "<localleader>b", ":FzfLua buffers<Enter>")
