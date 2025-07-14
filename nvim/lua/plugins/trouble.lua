2d836162cabbde796811e67dfe48684a34a0416a
Tyler Much <tylermuch@gmail.com>
2025-07-14 10:35:23 -0700

    Make symbols view take 30% of the window (trouble plugin)

---
+++ nvim/lua/plugins/trouble.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/trouble.nvim",
+  cmd = { "Trouble" },
+  opts = {
+    modes = {
+      symbols = {
+        win = {
+          type = "split",
+          relative = "win",
+          position = "right",
+          size = 0.3,
+        },
+      },
+    },
+  },
+}
