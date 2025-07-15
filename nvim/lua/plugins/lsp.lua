27145c7aa2db3ba5176558a542f94d71827af136
Tyler Much <tylermuch@gmail.com>
2025-07-14 17:48:25 -0700

    Fix clangd path finding

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,17 +1,17 @@
 -- Set the clangd path according to xcrun if we're running on macOS
 -- This is to try to silence warnings/errors stemming from build with an instance of clang from
 --   a mismatched toolchain.
-local clangd_path = ""
+local clangd_path = "clangd"
 local sysname = vim.loop.os_uname().sysname
 
 if sysname == "Darwin" then
-  local handle = io.popen('xcrun --show-toolchain-path')
+  local handle = io.popen('xcrun -f clangd')
   if handle == nil then return end
 
   local result = handle:read("*a")
   handle:close()
 
-  clangd_path = result:gsub("\n$", "") .. "/usr/bin/"
+  clangd_path = result:gsub("\n$", "")
 end
 
 return {
@@ -21,7 +21,7 @@ return {
     autoformat  = false,
     setup = {
       clangd = function (_, opts)
-      opts.cmd = { clangd_path .. "clangd", "--header-insertion=never"}
+      opts.cmd = { clangd_path, "--header-insertion=never"}
       end,
     }
   },
